5ac50a8e851b0c6a0e5f637aaba9fd35e62a23a7
mireia <mireia@actualweb.es>
2017-11-16 12:54:32 +0100

    iniciada app library

---
+++ library/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+
+class Book(models.Model):
+    author = models.CharField(max_length=250)
+    title = models.CharField(max_length=250)
+    description = models.TextField(null=True)
+    isbn = models.CharField(max_length=13, null=True)
+    year = models.IntegerField(null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
---
+++ library/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    #homepage
+    url(r'^$', views.index, name='index'),
+]
+
