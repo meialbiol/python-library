e2e00bc2f27b6cb24b7c22476c3c2581b2a0c74d
mireia <mireia@actualweb.es>
2018-01-24 15:36:33 +0100

    Codigo adaptado al tutorial de mozilla

--- library/urls.py
+++ library/urls.py
@@ -5,9 +5,11 @@ from . import views
 urlpatterns = [
     #homepage
     url(r'^$', views.index, name='index'),
-    url(r'^books/$', views.book_list, name='book_list'),
+    # url(r'^catalogue/books/$', views.book_list, name='book_list'),
+    url(r'^catalogue/books/$', views.BookListView.as_view(), name='book_list'),
+    url(r'^catalogue/book/(?P<pk>\d+)/$', views.BookDetailView.as_view(), name='show_book'),
+    # url('catalogue/book/<int:pk>', views.BookDetailView.as_view(), name='book_detail'),
     url(r'^new_book/$', views.new_book, name='new_book'),
-    url(r'^book/(?P<book_id>\d+)/$', views.show, name='show_book'),
     url(r'^book/edit/(?P<book_id>\d+)/$', views.edit, name='edit_book'),
 ]
 
--- library/views.py
+++ library/views.py
@@ -1,16 +1,31 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
 from django.urls import reverse
 
 from library.forms import BookForm
 from library.models import Book
+from library.models import BookInstance
+from library.models import Author
 
+
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = 'my_book_list'
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    context_object_name='book'
 
 def index(request):
-    return render(request, 'library/index.html')
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.all().count()
 
+    return render(request, 'library/index.html', context={'num_books': num_books, 'num_instances': num_instances, 'num_instances_available': num_instances_available, 'num_authors': num_authors})
 
 def book_list(request):
     books = Book.objects.all()
@@ -23,7 +38,12 @@ def new_book(request):
     else:
         form = BookForm(request.POST)
         if form.is_valid():
-            form.save()
+            book = form.save()
+            book_instance = BookInstance(
+                status='a',
+                book_id=book.id
+            )
+            book_instance.save()
             return HttpResponseRedirect(reverse('library:book_list'))
 
     context = {'form': form}
@@ -34,7 +54,7 @@ def show(request, book_id):
     book = Book.objects.get(id=book_id)
     context = {'book': book}
 
-    return render(request,'library/show.html', context)
+    return render(request, 'library/book_detail.html', context)
 
 
 def edit(request, book_id):
