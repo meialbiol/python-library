69500a3692655b653db597175e8809a7f318b5da
mireia <mireia@actualweb.es>
2018-01-24 11:02:35 +0100

    Añadida parte de administración y personalizada

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from .models import Book, BookInstance, Author
+
+
+class BookInline(admin.TabularInline):
+    model = Book
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
+
 
 # Register your models here.
+# admin / Actualmola05
+
+# admin.site.register(Book)
+# admin.site.register(BookInstance)
+admin.site.register(Author, AuthorAdmin)
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author')
+    inlines = [BookInstanceInline]
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book','status', 'due_back')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+           'fields': ('book', 'imprint')
+        }),
+        ('Availability', {
+           'fields': ('status', 'due_back')
+        }),
+    )
--- library/models.py
+++ library/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 
 # Create your models here.
 
 
 class Book(models.Model):
-    author = models.CharField(max_length=250)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=250)
     description = models.TextField(null=True)
     isbn = models.CharField(max_length=13, null=True)
@@ -17,3 +18,54 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class BookInstance(models.Model):
+    """
+    Represents the stat of a book
+    """
+
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
+    imprint = models.CharField(max_length=200,null=True, blank=True)
+    due_back= models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved')
+    )
+
+    status = models.CharField(max_length=1, choices=LOAN_STATUS, default='m', help_text='Available Book')
+
+    class Meta:
+        ordering = ["due_back"]
+
+    def __str__(self):
+        return '{0} ({1})'.format(self.id, self.book.title)
+
+
+class Author(models.Model):
+    """
+    Represents the Author Model
+    """
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField(null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        """
+        Returns the absolute url to an author
+        :return:
+        """
+
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+
+        return '{0}, {1}'.format(self.last_name, self.first_name)
