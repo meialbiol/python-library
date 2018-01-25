62e29522869cb8f0897f74fe931a682a6c629984
mireia <mireia@actualweb.es>
2018-01-25 11:25:03 +0100

    Añadida sesión número de visitas  de una página

--- library/views.py
+++ library/views.py
@@ -38,10 +38,14 @@ def index(request):
     num_instances = BookInstance.objects.all().count()
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.all().count()
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
 
-    return render(request, 'library/index.html', context={'num_books': num_books, 'num_instances': num_instances,
+    return render(request, 'library/index.html', context={'num_books': num_books,
+                                                          'num_instances': num_instances,
                                                           'num_instances_available': num_instances_available,
-                                                          'num_authors': num_authors})
+                                                          'num_authors': num_authors,
+                                                          'num_visits': num_visits})
 
 
 def book_list(request):
