f25c8dd107cbd8d672d8d73b8fbda2c9d2c7bed1
mireia <mireia@actualweb.es>
2018-01-25 12:55:39 +0100

    Añadida vista login con estilos propios

--- website/settings.py
+++ website/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['./templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
