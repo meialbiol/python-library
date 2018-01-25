c16288be8a7050aca1c0c04cd177610baaf37976
mireia <mireia@actualweb.es>
2018-01-25 10:22:33 +0100

    añadida propia hoja de estilos

--- library/views.py
+++ library/views.py
@@ -13,7 +13,7 @@ from library.models import Author
 
 class BookListView(generic.ListView):
     model = Book
-    paginate_by = 10
+    paginate_by = 5
     context_object_name = 'my_book_list'
 
 
