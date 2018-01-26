0f9d674b797804f29e554515cf156ca033cb89ca
mireia <mireia@actualweb.es>
2018-01-26 09:09:19 +0100

    añadido usuario al BookInstance

--- library/models.py
+++ library/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -37,6 +38,7 @@ class BookInstance(models.Model):
     )
 
     status = models.CharField(max_length=1, choices=LOAN_STATUS, default='m', help_text='Available Book')
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     class Meta:
         ordering = ["due_back"]
