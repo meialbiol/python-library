323e54819c5a0c17ba264387c383ccce9a469b41
mireia <mireia@actualweb.es>
2018-01-30 14:32:41 +0100

    Añadida validación

--- library/forms.py
+++ library/forms.py
@@ -1,5 +1,10 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
 from .models import Book
+
+import datetime
+
 
 class BookForm(forms.ModelForm):
     class Meta:
@@ -8,5 +13,21 @@ class BookForm(forms.ModelForm):
         widgets = {
             'description': forms.Textarea(attrs={'cols': 80, 'class': 'materialize-textarea'}),
             'year': forms.NumberInput(attrs={'min': '0', 'max': '9999', 'step': '1'})
-            }
+        }
 
+
+class RenewBookForm(forms.Form):
+    renewal_date = forms.DateField(help_text="Seleccione una fecha entra hoy y 4 semanas (por defecto 3 semanas)")
+
+    def clean_renewal_date(self):
+        data = self.cleaned_data['renewal_date']
+
+        # Chek date is not in the past
+        if data < datetime.date.today():
+            raise ValidationError(_('Invalida date - renewal in past'))
+
+        # Check date is in range librarian allowed to change (+4 weeks).
+        if data > datetime.date.today() + datetime.timedelta(weeks=4):
+            raise ValidationError(_('Invalida date - renewal more than 4 weeks ahead'))
+
+        return data
